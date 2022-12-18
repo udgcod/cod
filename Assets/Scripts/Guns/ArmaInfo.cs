5582068a361934803e3f0383bb6ccd766ad029c0
u1973246 <u1973246@campus.udg.edu>
2022-12-18 14:25:52 +0100

    disparar + aimImatge

---
+++ Assets/Scripts/Guns/ArmaInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmaInfo : MonoBehaviour
+{
+    public float dany = 10f;
+    public float distancia = 100f;
+    public bool automatica = false;
+    public float tempsEntreTir = 0.1f;
+    public float bales = 5f;
+    public float bales_restants = 5f;
+
+    public Sprite aimImage;
+}
